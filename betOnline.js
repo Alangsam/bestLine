0e380b9889156fe542f56fb663d017a3c7da059d
Alex <alangsam98@gmail.com>
2020-08-20 12:48:52 -0700

    fixed bovada error

--- betOnline.js
+++ betOnline.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const gtBets = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://www.bovada.lv/sports/basketball/nba");
+  await page.goto("https://www.bovada.lv/sports/basketball");
   await page.waitFor(5000);
   const games = await page.$$eval("h4.competitor-name", (nodes) =>
     nodes.map((n) => n.innerText)
@@ -17,32 +17,23 @@ const gtBets = async () => {
 
   const gameArr = await seeGames.split(",");
   const spreadArr = await seeSpread.split(",");
-  // let moneyArr = [];
-  // for (let i = 3; i < spreadArr.length; i += 6) {
-  //   moneyArr.push(spreadArr[i], spreadArr[i + 1]);
-  // }
-  // let totalArr = []
-  // for (let i = 3; i < spreadArr.length; i += 6) {
-  //   moneyArr.push(spreadArr[i], spreadArr[i + 1]);
-  // }
 
   let arr = [];
 
   for (let i = 0; i < gameArr.length; i += 2) {
-    let card = {
+    await arr.push({
       teams: [gameArr[i], gameArr[i + 1]],
-      spread: [spreadArr[i * 3], spreadArr[i * 3 + 1]],
-      money: [spreadArr[i * 3 + 2], spreadArr[i * 3 + 3]],
-      total: [spreadArr[i * 3 + 4], spreadArr[i * 3 + 5]],
-    };
-    arr.push(card);
+      spread: [spreadArr[i], spreadArr[i + 1]],
+      money: [spreadArr[i + 2], spreadArr[i + 3]],
+      total: [spreadArr[i + 4], spreadArr[i + 5]],
+    });
   }
 
   fs.writeFileSync("distB.json", JSON.stringify(arr));
   //await
   //     num += 1;
   //   }
-  console.log(gameArr, spreadArr);
+  console.log(arr);
   //await browser.close();
   //console.log();
 };
